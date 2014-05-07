c0f75afb2bfb0dd1664ec05ad561651961d3d45e
htakato <s13526ht@sfc.keio.ac.jp>
2014-05-08 07:03:49 +0900

    annimate.jsのコメント付けも一応終了

--- js/annimate.js
+++ js/annimate.js
@@ -1,7 +1,7 @@
 var endFlipFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 	foxkeh.removeEventListener("animationend", endFlipFoxkeh);
-
+	//前のイベントリスナーの削除
 	foxkeh.setAttribute("class", "");
 };
 
@@ -9,17 +9,21 @@ var endSlideinFoxkehAndStartFlipFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 
 	foxkeh.removeEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
+	//前のイベントリスナーの削除
 	foxkeh.addEventListener("animationend", endFlipFoxkeh);
-
+	//アニメーション終了とともにendFlipFoxkehを実行
 	foxkeh.setAttribute("class", "flip");
+	//前のアニメーション終了と同時にflip
 };
 
 var startSlideinFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 	foxkeh.setAttribute("class", "slidein");
-
+//slideinの実行
 	foxkeh.addEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
 };
+//アニメーション終了(CSSで設定された)とともにendSlideinFoxkehAndStartFlipFoxkeh実行
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+//clickするとstartSlideinFoxkehが実行される
