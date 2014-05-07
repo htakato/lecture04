453f6baeeb423514b293b3f9dcfa5b18b1f21ca7
htakato <s13526ht@sfc.keio.ac.jp>
2014-05-08 06:59:15 +0900

    slideinのぱーと一応できた

--- js/slidein.js
+++ js/slidein.js
@@ -3,13 +3,16 @@ var endSlideinFoxkeh = function(){
 
 	foxkeh.setAttribute("class", "");
 };
+//slidein終了
 
 var startSlideinFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 	foxkeh.setAttribute("class", "slidein");
-
+//スライドイン開始
 	foxkeh.addEventListener("animationend", endSlideinFoxkeh);
 };
+//endSlideinFoxkehを実行する
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+//ボタンを押すとstartSlideinFoxkehが実行される
